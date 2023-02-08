e021f6a49c88d18fa16bba2166e44f176e65bc61
DingWB <ding_wu_bin@163.com>
2023-02-07 23:09:37 -0500

    remove docs and data for distribution

--- setup.py
+++ setup.py
@@ -15,9 +15,11 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 # print(long_description)
 
+
+#release new version
 setup(
    name='PyComplexHeatmap',
-   version='1.2.3',
+   version='1.2.4',
    description='A Python package to plot complex heatmap',
    # long_description="#PyComplexHeatmap\n##Documentation:https://dingwb.github.io/PyComplexHeatmap/build/html/index.html",
    # long_description_content_type='text/markdown',
@@ -25,14 +27,9 @@ setup(
    author_email='ding.wu.bin.gm@gmail.com',
    url="https://github.com/DingWB/PyComplexHeatmap",
    packages=['PyComplexHeatmap'],
-   # package_dir={'':'PyComplexHeatmap'},
-   # packages=find_packages('PyComplexHeatmap'),
    # install_requires=['matplotlib>=3.3.1','pandas'],
-   data_files=[('Lib/site-packages/PyComplexHeatmap/data',['data/mammal_array.pkl'])]
-   #scripts=['scripts/PyComplexHeatmap'],
+   include_package_data=True,
 )
-
-#release new version
 # rm -rf dist && rm -rf PyComplexHeatmap/PyComplexHeatmap.egg-info/
 # python setup.py sdist bdist_wheel
 # twine upload dist/*
