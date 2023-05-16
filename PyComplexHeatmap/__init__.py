ba03972e25e68c6c4fb0bac3d62f91d7e775c572
DingWB <ding.wu.bin.gm@gmail.com>
2023-05-16 15:09:44 -0700

    upgrade to 1.4.4: run much more faster than previous version especially when there are multiple subgroups for row_split or col_split

--- PyComplexHeatmap/__init__.py
+++ PyComplexHeatmap/__init__.py
@@ -10,6 +10,6 @@ from .tools import *
 from .utils import set_default_style
 
 #__all__=['*']
-__version__='1.4.3'
+__version__='1.4.4'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = (this_directory / "README.md").read_text()
 #release new version
 setup(
    name='PyComplexHeatmap',
-   version='1.4.3',
+   version='1.4.4',
    description='A Python package to plot complex heatmap',
    # long_description="#PyComplexHeatmap\n##Documentation:https://dingwb.github.io/PyComplexHeatmap/build/html/index.html",
    # long_description_content_type='text/markdown',
