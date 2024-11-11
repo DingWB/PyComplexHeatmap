515524cf910f53adb46584105e9406e554a700c4
DingWB <ding.wu.bin.gm@gmail.com>
2024-11-10 19:53:02 -0800

    remove fastcluster from default requirements

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     author_email="ding.wu.bin.gm@gmail.com",
     url="https://github.com/DingWB/PyComplexHeatmap",
     packages=["PyComplexHeatmap"],  # src
-    install_requires=["matplotlib","numpy","pandas>=1.3.5", "scipy","fastcluster","palettable"],
+    install_requires=["matplotlib","numpy","pandas>=1.3.5", "scipy","palettable"],
     include_package_data=True,
 )
 # rm -rf dist && rm -rf PyComplexHeatmap/PyComplexHeatmap.egg-info/
